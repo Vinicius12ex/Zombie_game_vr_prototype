cf62a0f6d88714c0b72e8ee01c4d00b7d387c0b2
Vinicius12ex <vinicius12ex@hotmail.com>
2019-02-26 11:59:09 -0300

    Initial commit

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    [Header("Atirar")]
+    [Tooltip("Prefab do projétil que será gerado quando o jogador atirar")]
+    public GameObject projetilPrefab;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        bool botaoPressionado = Input.GetMouseButtonDown(0);
+        if (botaoPressionado)
+        {
+            Atirar();
+        }
+    }
+
+    private void Atirar()
+    {
+        GameObject projetil = Instantiate(projetilPrefab);
+        projetil.transform.position = Camera.main.transform.position;
+        projetil.transform.rotation = Camera.main.transform.rotation;
+    }
+}
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour
+{
+    [Range(1, 5)]
+    public float velocidade = 2f;
+
+    [Range(1, 30)]
+    public float Destruidor;
+
+    // Use this for initialization
+    private void Start()
+    {
+        StartCoroutine(Destruidor1(Destruidor));
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * velocidade * Time.deltaTime);
+    }
+
+    private IEnumerator Destruidor1(float Destruidor)
+    {
+        yield return new WaitForSeconds(Destruidor);
+        Destroy(gameObject);
+    }
+}
