12ce89d540dd6a274891228ca199bb1250ac535c
Vinicius <vinicius12ex@hotmail.com>
2019-03-19 09:19:09 -0300

    Destruição do projétil e zumbi
    Projétil e zumbi destruíveis agora

--- Assets/Scripts/Zumbi.cs
+++ Assets/Scripts/Zumbi.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class Zumbi : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Exibo a tag do collider trigger que ativou
+        print("OnTriggerEnter Zumbi: " + other.name);
+
+        //verificou se a tag do trigger é igual a projétil
+        if (other.CompareTag("Projetil"))
+        {
+            // Objetivo other verificado. É o projetil
+            // Destrói o gameObject do projétil
+            Destroy(other.gameObject);
+
+            // Destrói o zumbi que foi atingido
+            Destroy(gameObject);
+        }
+    }
 }
